c474aeb6b5278733479f1828a22ad2073b91f26b
Syl-gauthier <sylvain.gauthier.2012@gadz.org>
2017-04-24 23:22:24 +0900

    relocate members in this repository

---
+++ members/README.md
@@ -0,0 +1,2 @@
+# members
+daejeon programming group first small project. Here each ember can write his own html/css profile page to introduce himself.
---
+++ members/index.js
@@ -0,0 +1,16 @@
+var members = [ //name of the memenbers pages to create proper links
+  'sylvain'
+]
+
+members.sort(); //to get the list sorted alphabetically
+
+var pageList = document.getElementById('page-list');
+
+members.map(function(member) {
+  let linkElt = document.createElement('a');
+  linkElt.href = 'pages/' + member + '.html';
+  linkElt.textContent = member;
+  listElt = document.createElement('li');
+  listElt.appendChild(linkElt);
+  pageList.appendChild(listElt);
+});
